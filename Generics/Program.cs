5f29613d14f6794e28c187412164027378c29171
shubhampachghare <shubhampachghare@gmail.com>
2023-03-06 01:03:45 +0530

    [Shubham]-FindMaxNumber

---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GenericsProblem");
+
+            int maxIntNumber = MaximumNumberCheck.maximumIntegerNumber(500, 700, 600);
+            Console.WriteLine(maxIntNumber);
+        }
+    }
+}
